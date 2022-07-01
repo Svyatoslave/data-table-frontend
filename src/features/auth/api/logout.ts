339d09b6d8e89f2834c7b73b85fe7a8607a4d0f4
Svyatoslav <svyatoslave07@gmail.com>
2022-07-01 10:45:05 +0300

    removed unused files

--- src/features/auth/api/logout.ts
+++
@@ -1,3 +0,0 @@
-import { axios } from "@/lib/axios";
-
-export const logout = (): Promise<void> => axios.post("/auth/logout");
--- src/features/auth/types/index.ts
+++
@@ -1,6 +0,0 @@
-import type { User } from "@/features/users";
-
-export interface AuthUser {
-  token: string;
-  user: User;
-}
